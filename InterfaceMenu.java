78777a60d2a24a6aa67b80880583a4bde19e4580
Rafael Santos <rafarsk78@gmail.com>
2025-07-21 16:13:45 -0300

    Atualização da interface

--- InterfaceMenu.java
+++ InterfaceMenu.java
@@ -1,3 +1,13 @@
-public class InterfaceMenu {
-    
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class InterfaceMenu{
+    private GerenciadorTarefas gerenciadorTarefas;
+    private Scanner scanner;
+
+    public InterfaceMenu(){
+        this.gerenciadorTarefas = new GerenciadorTarefas();
+        this.scanner = new Scanner(System.in);
+    }
 }
